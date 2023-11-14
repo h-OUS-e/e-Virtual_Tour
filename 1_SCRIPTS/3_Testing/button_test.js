179e1711123d8b41c5c93eeb0c4a490d13647cb3
Aws Abu Ras <ous.abouras@gmail.com>
2023-11-14 02:27:19 -0500

    disabled icons while viewing other background img

--- 1_SCRIPTS/3_Testing/button_test.js
+++ 1_SCRIPTS/3_Testing/button_test.js
@@ -37,20 +37,85 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
             if (intersectedObject && intersectedObject.classList.contains('clickable')) {
                 intersectedObject.emit('hoverin'); // Emit custom hoverin event
-            }
+            }           
 
             lastHovered = intersectedObject;
         }
+
+        
     });
 
+    canvasEl.addEventListener('click', (event) => {
+        let raycaster = updateRaycaster(event);
+        let intersectedObject = checkIntersections(raycaster);
+
+        if (intersectedObject && intersectedObject.classList.contains('clickable')) {
+            intersectedObject.emit('mouseClicked');
+        }
+    });
+    
     // Define custom hoverin and hoverout event listeners
     sceneEl.addEventListener('hoverin', function (event) {
         const targetEl = event.target;
         targetEl.setAttribute('material', 'color', '#FFC0CB'); // Change color on hover
+
     });
+
 
     sceneEl.addEventListener('hoverout', function (event) {
         const targetEl = event.target;
         targetEl.setAttribute('material', 'color', '#4CC3D9'); // Revert color on hover out
+    });
+    
+
+    sceneEl.addEventListener('mouseClicked', (event) => {
+
+        if (event.target.classList.contains('clickable') && event.target.getAttribute('visible')) {
+            event.target.setAttribute('material', 'color', '#4CC3D9');
+
+
+        // Get the id of the clicked entity
+        var clickedId = event.target.id;
+        console.log('Clicked entity ID:', clickedId);
+        var obj = document.getElementById(clickedId);
+        var eventSetAttribute = obj.getAttribute('event-set__click');
+        var background_img_id = obj.getAttribute('background_img_id');
+        var new_background_img_id = obj.getAttribute('new_background_img_id');
+
+        // Extract the URL from the attribute value
+        var urlMatch = eventSetAttribute ? eventSetAttribute.match(/url\((.*?)\)/) : null;
+        var url = urlMatch ? urlMatch[1] : "URL not found";
+        console.log(url);
+
+
+        // Changing background image
+        var background_img = document.getElementById("background_img");
+        background_img.setAttribute('src', url);
+        background_img.setAttribute('background_img_id', new_background_img_id);
+        console.log('Moved to new scene!');
+
+        function toggleVisibility(selector, isVisible) {
+            const entities = document.querySelectorAll(selector);
+            entities.forEach(entity => {
+                entity.setAttribute('visible', isVisible);
+                if (isVisible) {
+                    entity.setAttribute('class', 'clickable');
+                } else {
+                    entity.setAttribute('class', 'unclickable');
+                }
+            });
+        }
+
+        // Hide the transition icons olf background
+        var selector = '[background_img_id="' + background_img_id + '"][myType="move"]';
+        toggleVisibility(selector, false);
+       
+
+        // show transition icon of new background
+        var selector2 = '[background_img_id="' + new_background_img_id + '"][myType="move"]';
+        // Iterate over the selected entities and hide them
+        toggleVisibility(selector2, true);
+    }
+
     });
 });
