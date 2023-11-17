5ad9c33604cdf45a9f5edafd21014e62dc8e9f32
h_ous_e <ous.abouras@gmail.com>
2023-11-17 13:25:40 -0500

    added a few images

---
+++ 1_SCRIPTS/3_Testing/popupVideo.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const scene = document.querySelector('a-scene');
+    const canvas = scene.canvas; // Define canvasEl here
+
+
+    scene.addEventListener('mouseClicked', (event) => {
+        
+        console.log(event.target);
+        
+        
+});
