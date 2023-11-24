77d37346d67ca78590e2dca37c4b8239fc5c78b8
h_ous_e <ous.abouras@gmail.com>
2023-11-24 14:20:38 -0500

    removed requirement to wait until background is loaded cuz it was causing bugs

--- 1_SCRIPTS/3_Testing/mediaPlayer.js
+++ 1_SCRIPTS/3_Testing/mediaPlayer.js
@@ -13,124 +13,95 @@ document.addEventListener('DOMContentLoaded', () => {
     const color_mediaPlayer = color_coolBlue
     const color_hoverInClicked = colors.getPropertyValue('--hoverInClicked').trim();
     
+    // Setting initial colors of objects
+    const entities = document.querySelectorAll('[class=' + main_class + ']');
+    entities.forEach(entity => {
+        entity.setAttribute('material', 'color', color_mediaPlayer);
+    });
 
 
     // Ensures that no objects are loaded before the sky is loaded
     document.querySelector('#sky').addEventListener('materialtextureloaded', function () {
-
-        // Setting initial colors of objects
-        scene.addEventListener('loaded', function () 
-        {
-            const entities = document.querySelectorAll('[class=' + main_class + ']');
-            entities.forEach(entity => {
-                entity.setAttribute('material', 'color', color_mediaPlayer);
-            });
-        });
+    });
 
 
-        // Changing color and scale of objects when hovering over them
-        scene.addEventListener('hoverin', function (event) 
-        {   
+    // Changing color and scale of objects when hovering over them
+    scene.addEventListener('hoverin', function (event) 
+    {   
 
-            if (event.target.classList.contains(main_class)){
-                event.target.setAttribute('material', 'color', color_hoverIn);
-                // const media_attachments_string = event.target.getAttribute('targets');
-                // const media_attachments = media_attachments_string.split(',').map(s => s.trim());
-                // var popupImage = document.getElementById(media_attachments[0]);
-                // popupImage.setAttribute('width', '30');
-                // popupImage.setAttribute('height', '18');
-                
-            }
-        });
-
-        // Resets color an scale of objects when hovering outside them
-        scene.addEventListener('hoverout', function (event) 
-        {
-            if (event.target.classList.contains(main_class)){
-                event.target.setAttribute('material', 'color', color_mediaPlayer); // Revert color on hover out
-                // const media_attachments_string = event.target.getAttribute('targets');
-                // const media_attachments = media_attachments_string.split(',').map(s => s.trim());
-                // var popupImage = document.getElementById(media_attachments[0]);
-                // popupImage.setAttribute('width', '5');
-                // popupImage.setAttribute('height', '3');
-            }
-
-        });
-
-
-        // Changing color of objects when hovering over them and clicking
-        scene.addEventListener('hoverin_mousedown', function (event) 
-        {
-            if (event.target.classList.contains(main_class)){
-                event.target.setAttribute('material', 'color', color_hoverInClicked);
-            }
-        });
-
-        // Changing color of objects when hovering over them and unclicking
-        scene.addEventListener('hoverin_mouseup', function (event) 
-        {
-        if (event.target.classList.contains(main_class))
-        {
-            event.target.setAttribute('material', 'color', color_mediaPlayer); 
+        if (event.target.classList.contains(main_class)){
+            event.target.setAttribute('material', 'color', color_hoverIn);
         }
-        });
+    });
+
+    // Resets color an scale of objects when hovering outside them
+    scene.addEventListener('hoverout', function (event) 
+    {
+        if (event.target.classList.contains(main_class)){
+            event.target.setAttribute('material', 'color', color_mediaPlayer); // Revert color on hover out
+        }
+
+    });
+
+
+    // Changing color of objects when hovering over them and clicking
+    scene.addEventListener('hoverin_mousedown', function (event) 
+    {
+        if (event.target.classList.contains(main_class)){
+            event.target.setAttribute('material', 'color', color_hoverInClicked);
+        }
+    });
+
+    // Changing color of objects when hovering over them and unclicking
+    scene.addEventListener('hoverin_mouseup', function (event) 
+    {
+    if (event.target.classList.contains(main_class))
+    {
+        event.target.setAttribute('material', 'color', color_mediaPlayer); 
+    }
+    });
+
+
+    
+
+    // Double clicking the object
+    scene.addEventListener('mouseDoubleClicked', function(event) {
+        if (event.target.classList.contains(main_class)){
+
+            // Create an event that sends media id when double clicked
+            var new_event = new CustomEvent('mediaPlayerDoubleClicked', 
+            {
+                detail: {id: event.target.id}
+            });
+            // Dispatch event
+            scene.dispatchEvent(new_event);
+            console.log("TEST2");
+            }
+            console.log("TEST");
+    });
+
 
 
         
 
-        // Double clicking the object
-        scene.addEventListener('mouseDoubleClicked', function(event) {
-            if (event.target.classList.contains(main_class)){
 
-                // Create an event that sends media id when double clicked
-                var new_event = new CustomEvent('mediaPlayerDoubleClicked', 
-                {
-                    detail: {id: event.target.id}
-                });
-                // Dispatch event
-                scene.dispatchEvent(new_event);
-                console.log("TEST2");
-             }
-             console.log("TEST");
-        });
+    // listen to mouseClicked event (it checks if click clicked on a clickable event)
+    scene.addEventListener('mouseClicked', (event) => 
+    {
 
-
-
-            
-
-
-        // listen to mouseClicked event (it checks if click clicked on a clickable event)
-        scene.addEventListener('mouseClicked', (event) => 
+        if ((event.target.getAttribute('visible')) && (event.target.classList.contains(main_class) || (event.target.classList.contains("popup_image")))) 
         {
-
-            if ((event.target.getAttribute('visible')) && (event.target.classList.contains(main_class) || (event.target.classList.contains("popup_image")))) 
+            // Create an event that sends media id when clicked
+            var new_event = new CustomEvent('mediaPlayerDoubleClicked', 
             {
-                // Create an event that sends media id when clicked
-                var new_event = new CustomEvent('mediaPlayerDoubleClicked', 
-                {
-                    detail: {id: event.target.id}
-                });
-                scene.dispatchEvent(new_event);
+                detail: {id: event.target.id}
+            });
+            scene.dispatchEvent(new_event);
 
-
-                // var mediaPlayer = event.target;
-                // const media_attachments_string = mediaPlayer.getAttribute('targets');
-                // const media_attachments = media_attachments_string.split(',').map(s => s.trim());
-                // var popupImage = document.getElementById(media_attachments[0]);
-                // var new_background_img_id =  mediaPlayer.getAttribute('new_background_img_id'); //get id of linked image
-                // console.log('test', new_background_img_id);
-                // console.log('test2', media_attachments);
-
-                
-                // // Toggle the visibility of the popup image
-                // const isVisible = popupImage.getAttribute('visible');
-                // popupImage.setAttribute('visible', !isVisible);
-                // console.log('Plane clicked!');
-
-            }
-        });
-
+        }
     });
+
+    
 });
 
 
--- 1_SCRIPTS/3_Testing/transitionNodes.js
+++ 1_SCRIPTS/3_Testing/transitionNodes.js
@@ -40,7 +40,13 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-//listen to minimapClick event
+    // Setting initial colors of objects
+    const entities = document.querySelectorAll('[class=' + main_class + ']');
+    entities.forEach(entity => {
+        entity.setAttribute('material', 'color', color_transitionNode);
+    });
+
+    //listen to minimapClick event
     scene.addEventListener('minimapClick', function(event) {
         var new_background_img_id = event.detail.new_background_img_id;
         console.log('New background image ID:', new_background_img_id);
@@ -62,99 +68,91 @@ document.addEventListener('DOMContentLoaded', () => {
         emitTransitioning(new_background_img_id);
         
     });
-
-
+    
 
 
     // Ensures that no objects are loaded before the sky is loaded
     document.querySelector('#sky').addEventListener('materialtextureloaded', function () {
-    
-        // Setting initial colors of objects
-        scene.addEventListener('loaded', function () 
-        {
-            const entities = document.querySelectorAll('[class=' + main_class + ']');
-            entities.forEach(entity => {
-                entity.setAttribute('material', 'color', color_transitionNode);
-            });
-        });
+        console.log('LOADDDED');
+    });
+       
 
-
-        // Changing color of objects when hovering over them
-        scene.addEventListener('hoverin', function (event) 
-        {   
-            if (event.target.classList.contains(main_class)){
-                event.target.setAttribute('material', 'color', color_hoverIn);
-            }
-        });
-
-
-        scene.addEventListener('hoverout', function (event) 
-        {
-            const target = event.target;
-            if (target.classList.contains(main_class)){
-                target.setAttribute('material', 'color', color_transitionNode); // Revert color on hover out
-            }
-        });
-
-
-
-        // Changing color of objects when hovering over them and unclicking
-        scene.addEventListener('hoverin_mouseup', function (event) 
-        {
-        if (event.target.classList.contains(main_class))
-        {
-            event.target.setAttribute('material', 'color', color_transitionNode); 
+    // Changing color of objects when hovering over them
+    scene.addEventListener('hoverin', function (event) 
+    {   
+        if (event.target.classList.contains(main_class)){
+            event.target.setAttribute('material', 'color', color_hoverIn);
         }
-        });
+    });
 
 
-        // Changing color of objects when hovering over them and clicking
-        scene.addEventListener('hoverin_mousedown', function (event) 
-        {
-            if (event.target.classList.contains(main_class)){
-                event.target.setAttribute('material', 'color', color_hoverInClicked);
-            }
-        });
+    scene.addEventListener('hoverout', function (event) 
+    {
+        const target = event.target;
+        if (target.classList.contains(main_class)){
+            target.setAttribute('material', 'color', color_transitionNode); // Revert color on hover out
+        }
+    });
 
 
-        // listen to mouseClicked event (it checks if click clicked on a clickable event)
-        scene.addEventListener('mouseClicked', (event) => 
-        {
-            // "visible" is a special attribute that is boolean, unlicke my made up "clickable" attribute.
-            // Thus, no need for === signs to check "visible" attribute truth.
-            if ((event.target.getAttribute('visible')) && (event.target.classList.contains(main_class)))  
-                {
-                
-                // Get the id of the clicked entity
-                
-                var clickedId = event.target.id;
-                var obj = document.getElementById(clickedId); //obj is the clickable thing that is clicked
-                var background_img_id = obj.getAttribute('background_img_id'); //current background image id
-                var new_background_img_id =  obj.getAttribute('new_background_img_id'); //get id of linked image
 
-                if (obj.classList.contains("transitionNode")){
-                    event.target.setAttribute('color', color_transitionNode); // resetting color on clicking
-                    console.log('id', new_background_img_id);
+    // Changing color of objects when hovering over them and unclicking
+    scene.addEventListener('hoverin_mouseup', function (event) 
+    {
+    if (event.target.classList.contains(main_class))
+    {
+        event.target.setAttribute('material', 'color', color_transitionNode); 
+    }
+    });
 
-                    // Emit the transitioning event to change the background image and minimap highlights
-                    emitTransitioning(new_background_img_id) 
-                };
-                // Hide the transition icons old background
-                var selector = '[background_img_id="' + background_img_id + '"]'; //background image is the image clicked from, type moved
-                toggleVisibility(selector, false);       
 
-                // show transition icon of new background
-                var selector2 = '[background_img_id="' + new_background_img_id + '"][visible_on_load="true"]'; //background image is the new image we are clicking to, type moved
-                // Iterate over the selected entities and hide them
-                toggleVisibility(selector2, true);
+    // Changing color of objects when hovering over them and clicking
+    scene.addEventListener('hoverin_mousedown', function (event) 
+    {
+        if (event.target.classList.contains(main_class)){
+            event.target.setAttribute('material', 'color', color_hoverInClicked);
+        }
+    });
 
-                // can we wrap the selector creation, visibility toggle and background toggle in one big function?
-                // that way we can call on it from other events. K.T
-            };
 
-        });
+    // listen to mouseClicked event (it checks if click clicked on a clickable event)
+    scene.addEventListener('mouseClicked', (event) => 
+    {
+        // "visible" is a special attribute that is boolean, unlicke my made up "clickable" attribute.
+        // Thus, no need for === signs to check "visible" attribute truth.
+        if ((event.target.getAttribute('visible')) && (event.target.classList.contains(main_class)))  
+            {
             
+            // Get the id of the clicked entity
+            
+            var clickedId = event.target.id;
+            var obj = document.getElementById(clickedId); //obj is the clickable thing that is clicked
+            var background_img_id = obj.getAttribute('background_img_id'); //current background image id
+            var new_background_img_id =  obj.getAttribute('new_background_img_id'); //get id of linked image
+
+            if (obj.classList.contains("transitionNode")){
+                event.target.setAttribute('color', color_transitionNode); // resetting color on clicking
+                console.log('id', new_background_img_id);
+
+                // Emit the transitioning event to change the background image and minimap highlights
+                emitTransitioning(new_background_img_id) 
+            };
+            // Hide the transition icons old background
+            var selector = '[background_img_id="' + background_img_id + '"]'; //background image is the image clicked from, type moved
+            toggleVisibility(selector, false);       
+
+            // show transition icon of new background
+            var selector2 = '[background_img_id="' + new_background_img_id + '"][visible_on_load="true"]'; //background image is the new image we are clicking to, type moved
+            // Iterate over the selected entities and hide them
+            toggleVisibility(selector2, true);
+
+            // can we wrap the selector creation, visibility toggle and background toggle in one big function?
+            // that way we can call on it from other events. K.T
+        };
 
     });
+            
+
+    
 
 });
