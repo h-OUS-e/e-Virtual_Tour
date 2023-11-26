e49a7328acaa99c0622658223232c5ab39c36ccd
h_ous_e <ous.abouras@gmail.com>
2023-11-26 15:43:14 -0500

    added popup content

--- 1_SCRIPTS/3_Testing/popupContent.js
+++ 1_SCRIPTS/3_Testing/popupContent.js
@@ -1,6 +1,18 @@
 const popupContent = [
   {
     media_id: "mediaplayer_1",
+    title: "Advanced Patient Diagnostics",
+    subtitle: "SageCare",
+    description: "",
+    bodyText: "Sage Dental has turned the entire patient experience into a digital discovery session. We believe patients and providers should no longer have a disjointed, paper-filled registration and treatment planning process.  Patients live with their smart phones, so we meet them where they live.  <br> Sage utilizes the very best technology, such as iOS scanners to avoid uncomfortable impressions and intelligent X-rays to clearly illustrate care opportunities.    Sage patients have the easiest, most comfortable dental visit. The Sage Digital Cycle creates trust and clarity for patients, and efficiency and buy-in for providers, resulting in the easiest and most comfortable patient and provider experience..",
+    imageUrl: "0_resources/media_popups/diagnostics.png",
+    videoUrl: "",
+    videoUrlEmbedded: "https://player.vimeo.com/video/738257130?h=a37b9df869&byline=0&portrait=0",
+    color_class: "pink",
+    icon_index: "1"
+  },
+  {
+    media_id: "mediaplayer_2",
     title: "Integrated Artificial Intelligence",
     subtitle: "SageCare",
     description: "AI-enhanced X-rays by Sage enable quicker, more accurate diagnoses.",
@@ -8,21 +20,56 @@ const popupContent = [
     imageUrl: "0_resources/media_popups/Intelligent-X-Ray.png",
     videoUrl: "",
     videoUrlEmbedded: "https://player.vimeo.com/video/738257760?h=c948825763&byline=0&portrait=0",
+    color_class: "blue",
+    icon_index: "2"
+  },
+  {
+    media_id: "mediaplayer_3",
+    title: "Superior Revenue Cycle Management",
+    subtitle: "BillRIGHT",
+    description: "",
+    bodyText: "BillRIGHT is the most comprehensive insurance management and revenue cycle operations software platform in Dental today. <br> By combining third-party data sources with proprietary logic, BillRIGHT has streamlined the entire insurance verification process. Sage reduced the manual workload on both the practice and the backend support team. <br>  Since launch, Sage has increased insurance estimation and first-time claim accuracy significantly; increased patient collections by $6.2 million dollars annually; and reduced revenue cycle expense per practice by 20 percent.   ",
+    imageUrl: "0_resources/media_popups/revenue.png",
+    videoUrl: "",
+    videoUrlEmbedded: "https://player.vimeo.com/video/738257641?h=56288e389c&byline=0&portrait=0",
     color_class: "green",
     icon_index: "1"
   },
   {
-    media_id: "mediaplayer_2",
-    title: "Title 2",
-    subtitle: "Subtitle 2",
-    description: "Description for Title 2",
-    bodyText: "Content for Title 2...",
+    media_id: "mediaplayer_4",
+    title: "SageNet Automated Purchasing System",
+    subtitle: "SNAP",
+    description: "",
+    bodyText: "With SNAP, SageNet Automating Purchasing System, office staff who used to spend hours ordering supplies, visiting hundreds of suppliers’ websites, and waiting for approval while managing separate budgets, can now manage and track all of this from one place. <br> SNAP has integrated nearly 90% of its annual dental supply purchases into a singular digital catalog – aggregated from over 100 suppliers. Practices can quickly find and order supplies, manage budgets and provider preferences, identify cost-saving opportunities, and automate the approval process.",
+    imageUrl: "0_resources/media_popups/snap.png",
+    videoUrl: "",
+    videoUrlEmbedded: "",
+    color_class: "green",
+    icon_index: "1"
+  },
+  {
+    media_id: "mediaplayer_5",
+    title: "Actionable Business Intelligence",
+    subtitle: "VISION",
+    description: "With SAGE VISION, office staff, regional managers, and executive leadership can access real-time key metrics in a format that makes the most sense for their individual needs and perspectives.",
+    bodyText: "Office managers can manage the complete dental practice from a single screen,  Regional Managers, and Clinical Leaders can explore where and how they need to focus their time and attention, and executive leadership can review the entire business lifecycle from the highest enterprise view,  through to the payor mix, procedure code, or general ledger of a patient.<br> With integrated functions and automated workstreams, VISION is designed to turn the intelligence generated from vision into actionable performance executions.",
+    imageUrl: "",
+    videoUrl: "",
+    videoUrlEmbedded: "https://player.vimeo.com/video/738257760?h=c948825763&byline=0&portrait=0",
+    color_class: "blue",
+    icon_index: "1"
+  },
+  {
+    media_id: "mediaplayer_6",
+    title: "Interactive Patient Management",
+    subtitle: "SEAT CRM",
+    description: "",
+    bodyText: "From easy online scheduling, remote consultations, digital patient paperwork, interactive appointment reminders, dedicated patient chat, online bill-pay, treatment reminders, and more, we are dedicated to providing our patients with the very best experience to make their dental journey seamless.<br>Sit back, relax and enjoy a cup of coffee, because at Sage Dental, we have you covered no matter where you are and what you are doing.",
     imageUrl: "",
     videoUrl: "",
     videoUrlEmbedded: "",
     color_class: "pink",
     icon_index: "1"
-  }
-  //0_resources/12273497-4k.mp4
-  //https://player.vimeo.com/video/738257760?h=c948825763&byline=0&portrait=0
+  },
+
 ];
