a6b559679f98bb554450f89550e94e69b2ef0820
Ous Abou Ras <66432003+h-OUS-e@users.noreply.github.com>
2023-12-16 18:03:51 -0500

    clicking on scene hides dashboard items. Added dashboard.js

--- 1_SCRIPTS/3_Testing/assets/js/dashboard.js
+++ 1_SCRIPTS/3_Testing/assets/js/dashboard.js
@@ -1,10 +1,20 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    // Define html blocks to show or hide related to dashboard
+    var minimap = document.getElementById('minimap_container');
+    var menu = document.getElementById('menu_container');
+    var menu_content = document.getElementById('menu_content');
+    var scene = document.getElementById('sky');
+    var mediabar_container = document.getElementById('mediabar_container');
+    var dashboard2 = document.getElementById('dahsboard');
+
+
     // FULLSCREEN
     document.getElementById('fullscreen_button').addEventListener('click', function() {
         if (!document.fullscreenElement) {
             document.documentElement.requestFullscreen();
+
         } else {
             if (document.exitFullscreen) {
                 document.exitFullscreen();
@@ -12,10 +22,10 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+
     // BURGER MENU
     document.getElementById('burger_button').addEventListener('click', function() {
-        var menu = document.getElementById('menu_container');
-        var menu_content = document.getElementById('menu_content');
+        
         if (menu.style.width === '250px') {
             menu.style.width = '0';
             menu_content.style.width = '0';
@@ -25,9 +35,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+
     // MINIMAP
     document.getElementById('minimap_button').addEventListener('click', function() {
-        var minimap = document.getElementById('minimap_container');
         if (minimap.style.width === '0px' || minimap.style.width === '') {
             minimap.style.width = '300px';
             minimap.style.height = '200px';
@@ -36,6 +46,25 @@ document.addEventListener('DOMContentLoaded', () => {
             minimap.style.height = '0px';
         }
     });
+
+
+    function hideDashboardItems() {
+        minimap.style.width = '0px';
+        minimap.style.height = '0px';
+        menu.style.width = '0';
+        menu_content.style.width = '0';
+    }
+
+
+    document.addEventListener('click', function(event) {
+        console.log(event.target);
+    });
+
+
+
+    scene.addEventListener('click', hideDashboardItems);
+
+
 
     // ICON GALLERY
 
