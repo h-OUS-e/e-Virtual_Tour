fc14a8c668232c47b797a1e419f435811b946f72
Ous Abou Ras <66432003+h-OUS-e@users.noreply.github.com>
2024-03-03 03:40:09 -0500

    fixed little bug where white screen and objects appear before scene is loaded

--- 1_SCRIPTS/3_Testing/static/js/sky.js
+++ 1_SCRIPTS/3_Testing/static/js/sky.js
@@ -17,21 +17,10 @@ document.addEventListener('DOMContentLoaded', async () => {
     // listen to transitioning event. emmited from transitionNode.js
     scene.addEventListener('transitioning', function (event) 
     {
-        // Changing image of the scene
-        
-        const current_background_img_id = sky.getAttribute('background_img_id');
+        // Changing image of the scene        
         var new_background_img_id = event.detail.new_background_img_id;
         changeScene(sky, new_background_img_id, scenes);
         // console.log('New background image ID transitioning:', new_background_img_id, typeof new_background_img_id);
-
-        // Hide the objects in old background
-        var selector = '[background_img_id="' + current_background_img_id + '"]'; //background image is the image clicked from, type moved
-        toggleVisibility(selector, false); 
-
-        // Show objects in new background
-        var selector2 = '[background_img_id="' + new_background_img_id + '"]'; //background image is the new image we are clicking to, type moved
-        // Iterate over the selected entities and hide them
-        toggleVisibility(selector2, true);
 
     });
 
@@ -90,39 +79,49 @@ function updateScene(sky, rotation, scenes) {
 
 // Checks if the scene image and adds to assets if they don't exist there already
 function loadImageAsset(asset_object) {
-    const assets = document.querySelector('a-assets');
-    if (!assets) {
-        console.error('a-assets element not found.');
-        return;
-    }
+    return new Promise((resolve, reject) => {
+        const assets = document.querySelector('a-assets');
+        if (!assets) {
+            console.error('a-assets element not found.');
+            reject('a-assets element not found.');
+            return;
+        }
 
-    // Attempt to find an existing asset with the given background_img_id
-    const existing_asset = assets.querySelector(`img[background_img_id="${asset_object.background_img_id}"]`);
-    
-    if (existing_asset) {
-        // console.log(`Asset with background_img_id: ${asset_object.background_img_id} already exists. Using existing asset.`);
-        return existing_asset;
+        // Attempt to find an existing asset with the given background_img_id
+        const existing_asset = assets.querySelector(`img[background_img_id="${asset_object.background_img_id}"]`);
+        
+        if (existing_asset) {
+            // console.log(`Asset with background_img_id: ${asset_object.background_img_id} already exists. Using existing asset.`);
+            resolve(existing_asset); // Asset already loaded
 
-    } else {
-        // Asset does not exist, so create and append a new image element
-        const img_element = document.createElement('img');
-        img_element.setAttribute('id', asset_object.id);
-        img_element.setAttribute('src', asset_object.path);
-        img_element.setAttribute('alt', asset_object.description || 'image of the scene');
-        img_element.setAttribute('description', asset_object.description);
-        img_element.setAttribute('background_img_id', asset_object.id);
-        img_element.setAttribute('rotation', asset_object.rotation);
+        } else {
+            // Asset does not exist, so create and append a new image element
+            const img_element = document.createElement('img');
+            img_element.classList.add('img-loading'); // Start with image fully transparent
+            img_element.setAttribute('id', asset_object.id);
+            img_element.setAttribute('src', asset_object.path);
+            img_element.setAttribute('alt', asset_object.description || 'image of the scene');
+            img_element.setAttribute('description', asset_object.description);
+            img_element.setAttribute('background_img_id', asset_object.id);
+            img_element.setAttribute('rotation', asset_object.rotation);
 
-        img_element.onerror = () => {
-            console.error(`Failed to load asset with background_img_id: ${asset_object.background_img_id}.`);
-            // Handle loading errors
-        };
+            // Apply the initial loading class for transparency
+            img_element.classList.add('img-loading');
 
-        assets.appendChild(img_element);
-        console.log(`Loaded new asset for background_img_id: ${asset_object.id}.`);
+            img_element.onload = () => {
+                console.log(`New asset for background_img_id: ${asset_object.background_img_id} loaded successfully.`);
+                assets.appendChild(img_element);
+                resolve(img_element);
+            };
 
-        return img_element;
-    }
+            img_element.onerror = () => {
+                // Handle loading errors
+                console.error(`Failed to load asset with background_img_id: ${asset_object.background_img_id}.`);
+                reject(`Failed to load asset with background_img_id: ${assetObject.background_img_id}.`);
+
+            };
+        }
+    });
 }
 
 
@@ -155,7 +154,7 @@ function toggleVisibility(selector, isVisible) {
 }
 
 
-function changeScene(sky, new_background_img_id, scenes)
+async function changeScene(sky, new_background_img_id, scenes)
 {
     // change background image
     // input: new_background_img_id: string & background_img_id: string
@@ -163,13 +162,27 @@ function changeScene(sky, new_background_img_id, scenes)
 
     // Find the scene object with the specified image ID, and add to asset if it don't exist
     const scene = scenes.find(scene => scene.background_img_id === new_background_img_id);
-    console.log("SCENE", scene)
-    let existing_asset = loadImageAsset(scene);
+    const current_background_img_id = sky.getAttribute('background_img_id');
     
-    // Change background image
-    sky.setAttribute('src', '#scene_' + new_background_img_id); 
-    sky.setAttribute('background_img_id', new_background_img_id);
-    sky.setAttribute('rotation', existing_asset.getAttribute('rotation'));
-    
-    console.log('Moved to new scene!', sky.getAttribute('src'));
-} 
+    try {
+        let existing_asset = await loadImageAsset(scene);
+        
+        // Once the image is loaded, update the <a-sky> element
+        sky.setAttribute('src', '#scene_' + new_background_img_id); 
+        sky.setAttribute('background_img_id', new_background_img_id);
+        sky.setAttribute('rotation', existing_asset.getAttribute('rotation'));        
+        console.log('Moved to new scene!', sky.getAttribute('src'));
+
+        // Hide the objects in old background
+        var selector = '[background_img_id="' + current_background_img_id + '"]'; //background image is the image clicked from, type moved
+        toggleVisibility(selector, false); 
+
+        // Show objects in new background
+        var selector2 = '[background_img_id="' + new_background_img_id + '"]'; //background image is the new image we are clicking to, type moved
+        // Iterate over the selected entities and hide them
+        toggleVisibility(selector2, true);
+
+    } catch (error) {
+        console.error('Error loading image:', error);
+    }
+}
