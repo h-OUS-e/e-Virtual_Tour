1250f382344cb31202800ef50633c63cc1fef493
KTayy <ktayfour97@gmail.com>
2024-03-06 18:45:18 -0500

    restructuring and importing

--- 1_SCRIPTS/3_Testing/static/js/dbAuthHandler.js
+++ 1_SCRIPTS/3_Testing/static/js/dbAuthHandler.js
@@ -1,17 +1,15 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-//test
-const supabaseUrl = 'https://ngmncuarggoqjwjinfwg.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5nbW5jdWFyZ2dvcWp3amluZndnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg1MzU3NzcsImV4cCI6MjAyNDExMTc3N30.kT57r1txjFS1bWZOWIuzLeJ5k2yYi3W8q5tw8JfuXlI';
+import { supabase } from "./dbEvents.js";
 
 
-export const supabase = createClient(supabaseUrl,supabaseAnonKey);
-
+console.log('from authhandler');
+console.log(supabase);
 // Handle the login form submission
 document.getElementById('login-form').addEventListener('submit', async function(event) {
     event.preventDefault();
 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
+    console.log(email, password);
 
     // initialize supabase client, basically supabase is the dude
     try {
--- 1_SCRIPTS/3_Testing/static/js/dbEvents.js
+++ 1_SCRIPTS/3_Testing/static/js/dbEvents.js
@@ -1,3 +1,13 @@
+// client initialization
+console.log('client initialization');
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+const supabaseUrl = 'https://ngmncuarggoqjwjinfwg.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5nbW5jdWFyZ2dvcWp3amluZndnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg1MzU3NzcsImV4cCI6MjAyNDExMTc3N30.kT57r1txjFS1bWZOWIuzLeJ5k2yYi3W8q5tw8JfuXlI';
+
+export const supabase = createClient(supabaseUrl,supabaseAnonKey);
+console.log(supabase)
+
+
 //=======event emmiter functions
 //emmit event functions
 export function emitGETProjectDataEvent(project_id) {
