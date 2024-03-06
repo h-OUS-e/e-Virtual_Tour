67e9efbfcab49c35e3d70f58e9b1c2844cd2b8b9
khaled <ktayfour97@gmail.com>
2024-03-06 09:30:50 -0500

    test

--- 1_SCRIPTS/3_Testing/static/js/authHandler.js
+++ 1_SCRIPTS/3_Testing/static/js/authHandler.js
@@ -1,5 +1,5 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-
+//test
 const supabaseUrl = 'https://ngmncuarggoqjwjinfwg.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5nbW5jdWFyZ2dvcWp3amluZndnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg1MzU3NzcsImV4cCI6MjAyNDExMTc3N30.kT57r1txjFS1bWZOWIuzLeJ5k2yYi3W8q5tw8JfuXlI';
 
