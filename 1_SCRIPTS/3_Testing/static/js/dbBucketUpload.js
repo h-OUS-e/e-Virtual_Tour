086c642f3ec698586f8e6ad9c00de8b02b125bcb
KTayy <ktayfour97@gmail.com>
2024-03-29 17:40:52 -0400

    uppy instance

--- 1_SCRIPTS/3_Testing/static/js/dbBucketUpload.js
+++ 1_SCRIPTS/3_Testing/static/js/dbBucketUpload.js
@@ -3,49 +3,62 @@ import Dashboard from '@uppy/dashboard';
 
 import '@uppy/core/dist/style.min.css';
 import '@uppy/dashboard/dist/style.min.css';
+import Tus from "@uppy/tus";
 import { supabase } from "./dbClient.js";
 
 
+const SUPABASE_ANON_KEY = 'replace-with-your-anon-key'
+const SUPABASE_PROJECT_ID = 'replace-with-your-project-id'
+const STORAGE_BUCKET = 'replace-with-your-bucket-id'
+const BEARER_TOKEN='replace-with-your-bearer-token'
+
+const folder = ''
+const supabaseStorageURL = `https://${SUPABASE_PROJECT_ID}.supabase.co/storage/v1/upload/resumable`
+
 //docs
+//https://github.com/supabase/supabase/blob/master/examples/storage/resumable-upload-uppy/index.html
+//https://www.youtube.com/watch?v=JLaq0x9GbbY
 //https://uppy.io/docs/dashboard/
 //https://www.restack.io/docs/supabase-knowledge-supabase-storage-metadata
 //https://www.restack.io/docs/supabase-knowledge-supabase-postgres-meta-guide#clpzdl7tp0lkdvh0v9gz12dc0
 
-new Uppy().use(Dashboard, { inline: true, target: '#uppy-dashboard' });
+const uppy = new Uppy()
+        .use(Dashboard, {
+          inline: true,
+          limit: 10,
+          target: '#drag-drop-area',
+          showProgressDetails: true,
+        })
+        .use(Tus, {
+          endpoint: supabaseStorageURL,
+          headers: {
+            authorization: `Bearer ${BEARER_TOKEN}`,
+            apikey: SUPABASE_ANON_KEY,
+          },
+          uploadDataDuringCreation: true,
+          chunkSize: 6 * 1024 * 1024,
+          allowedMetaFields: ['bucketName', 'objectName', 'contentType', 'cacheControl'],
+          onError: function (error) {
+            console.log('Failed because: ' + error)
+          },
+        })
 
-export async function uploadFile(file,file_name, bucket_name, project_uid, is_public) {
-  const randomUUID = crypro.randomUUID()
-  const file_path = `${project_uid}/${randomUUID}`
-
-  if (bucket_name == 'icons_img'){
-  
-  };
-
-  if (bucket_name == "scenes_img"){
-
-  };
-
-  try {
-    const { data, error } = await supabase
-    .storage
-    .from(bucket_name)
-    .upload(file_path, file, {
-      cacheControl: '3600',
-      upsert: false,
-      metadata: { 
-          project_uid: project_uid,
-          is_public: is_public,
-          file_name: file_name
+      uppy.on('file-added', (file) => {
+        const supabaseMetadata = {
+          bucketName: STORAGE_BUCKET,
+          objectName: folder ? `${folder}/${file.name}` : file.name,
+          contentType: file.type,
         }
-      });
 
-    if (error) {
-      console.log('error on upload ', error);
-      return error;
-    } else {
-      return data;
-    }
+        file.meta = {
+          ...file.meta,
+          ...supabaseMetadata,
+        }
 
-  } catch (error) { return error; }
-};
+        console.log('file added', file)
+      })
+
+      uppy.on('complete', (result) => {
+        console.log('Upload complete! We’ve uploaded these files:', result.successful)
+      })
 
