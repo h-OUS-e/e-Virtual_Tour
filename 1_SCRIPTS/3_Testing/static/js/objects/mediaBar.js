219a3a8299abd0869c10a4663e84d6034ff87f58
Ous Abou Ras <66432003+h-OUS-e@users.noreply.github.com>
2024-04-29 09:27:33 -0400

    Fixed Mediabar

--- 1_SCRIPTS/3_Testing/static/js/objects/mediaBar.js
+++ 1_SCRIPTS/3_Testing/static/js/objects/mediaBar.js
@@ -1,31 +1,73 @@
 /*
 A script to control what shows on the scroll bar based on popup contents.
 */
-const sidebar_styles = getComputedStyle(document.documentElement);
-const sidebar_width = sidebar_styles.getPropertyValue('--sidebar_width').trim();
+// LOADING JSON STATE
+import { JSON_statePromise } from '../JSONSetup.js';
+
+// GLOBAL CONSTANTS
+const MEDIABAR_styles = getComputedStyle(document.documentElement);
+const MEDIABAR_WIDTH = MEDIABAR_styles.getPropertyValue('--sidebar_width').trim();
+// Getting mediabar elements    
+const MEDIABAR = document.getElementById('sidebar');
+const MEDIABAR_ITEM_CONTAINER = MEDIABAR.querySelector('[id=sidebar-item-grid]');
 
 
-document.addEventListener('jsonLoaded', async () => {
+/*********************************************************************
+ * EVENT LISTENERS
+*********************************************************************/
+document.addEventListener('DOMContentLoaded', async () => {
+
+    /*********************************************************************
+     * 1. LOAD MEDIABAR ITEMS 
+    *********************************************************************/
+    // Load JSON state 
+    let {project_state, object_state} = await JSON_statePromise;
+
+    // Get mediaplayers to extract mediabar information
+    let mediaplayer_JSON = object_state.getCategory('MediaPlayers');
+    let types = project_state.getCategory('Types');
+    let icons = project_state.getCategory('Icons');
     
-    // Getting mediabar elements
-    
-    const sidebar = document.getElementById('sidebar');
 
-    const mediabar_item_container = document.querySelector('[id=sidebar-item-grid]');
+    loadMediabarFromJSON(mediaplayer_JSON, types, icons);
 
-    // Defining new mediabar items and posting them based on popup contents
-    popupContent.forEach(item => {
-        let entity = document.getElementById(item.media_id);
+
+    // Disabling zoom when zooming on menu
+  if (MEDIABAR) {
+    MEDIABAR.addEventListener('mouseenter', window.disableZoom);
+    MEDIABAR.addEventListener('mouseleave', window.enableZoom);
+  }
+
+});
+
+async function loadMediabarFromJSON(mediaplayer_JSON, types, icons) {
+    // Get an array of keys from the transitionNode_JSON object
+    const ids = Object.keys(mediaplayer_JSON);
+
+    // Iterate over the keys
+    ids.forEach((id) => {
+        const mediaPlayer_item = mediaplayer_JSON[id];
+
+        // Get attributes
+        const type_uuid = mediaPlayer_item.type_uuid;
+        const mediaplayer_type = types[type_uuid];
+        const title = mediaPlayer_item.title;
+        const description = mediaPlayer_item.description;
+        const scecne_id = mediaPlayer_item.scecne_id;
+        const icon_index = mediaPlayer_item.icon_index;
+        const icon_url = icons[mediaplayer_type["icons"][icon_index]].src;
         
+
         const media_element_title = document.createElement('div');              
          // Create the title element and edit color of title based on popup color
         media_element_title.innerHTML = `
-            <h4>${item.title}</h4>
+            <h4>${title}</h4>
         `;
+
         // Create the body element
         const media_element_body = document.createElement('div'); 
         media_element_body.innerHTML = `
-            <p>${item.description}</p>
+            <p>${description}</p>
         `;
         
         // Append the title and body to the media_element
@@ -33,57 +75,52 @@ document.addEventListener('jsonLoaded', async () => {
         media_element.classList.add('sidebar-item');
         media_element.appendChild(media_element_title);
         media_element.appendChild(media_element_body);
-
         
 
         // Change sky image when mouse is clicked and show popup
         media_element.addEventListener('click', function(event) {
 
-            var transitioning = new CustomEvent('mediabarItemClicked', {
-                detail: {new_background_img_id: entity.getAttribute('background_img_id'), 
-                    id: item.media_id},       
+            const transitioning = new CustomEvent('mediabarItemClicked', {
+                detail: {
+                    new_scene_id: scecne_id, 
+                    id: id
+                },       
             });
             scene.dispatchEvent(transitioning)
         });
 
         media_element.addEventListener('dblclick', function(event) {
-            var transitioning = new CustomEvent('mediabarItemDoubleClicked', {
+            const transitioning = new CustomEvent('mediabarItemDoubleClicked', {
                 detail: {
-                    new_background_img_id: entity.getAttribute('background_img_id'), 
-                    id: item.media_id,  
-                    mediaplayer_type: entity.getAttribute('mediaplayer_type') }
+                    new_scene_id: scecne_id, 
+                    id: id,  
+                    mediaplayer_type: mediaplayer_type
+                }
             });
             scene.dispatchEvent(transitioning)
         });
 
         // add media element to media container div
-        mediabar_item_container.appendChild(media_element);
-    
+        MEDIABAR_ITEM_CONTAINER.appendChild(media_element);
+        
     });
-
-
-    // Disabling zoom when zooming on menu
-  if (sidebar) {
-    sidebar.addEventListener('mouseenter', window.disableZoom);
-    sidebar.addEventListener('mouseleave', window.enableZoom);
-  }
-
-});
+    
+    // document.dispatchEvent(new CustomEvent('mediaBarLoaded'));
+}
 
     // A side toggle to hide or show the mediabar
     document.getElementById('sidebar-toggler-button').addEventListener('click', function() {
 
-        var sidebar = document.getElementById('sidebar');
-        var sidebarContainer = document.getElementById('sidebar-container');
+        const mediabar_container = document.getElementById('sidebar-container');
     
-        if (sidebar.style.width === '0%') {
+        if (MEDIABAR.style.width === '0%') {
             
-            sidebarContainer.style.width = '30%';
-            sidebar.style.width = '100%';
+            mediabar_container.style.width = '30%';
+            MEDIABAR.style.width = '100%';
         } else {
-            sidebarContainer.style.width = sidebar_width ;
+            mediabar_container.style.width = MEDIABAR_WIDTH;
             
-            sidebar.style.width = '0%';
+            MEDIABAR.style.width = '0%';
             
         }
     });
