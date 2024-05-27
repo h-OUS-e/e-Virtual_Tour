e53b688c18bd9bf888b280735066b1ddbe3a54e3
KTayy <ktayfour97@gmail.com>
2024-05-27 18:48:10 -0400

    project creation complete

--- 1_SCRIPTS/3_Testing/static/js/create_project.js
+++ 1_SCRIPTS/3_Testing/static/js/create_project.js
@@ -1,7 +1,19 @@
-import { insertProject } from '../js/db/dbEvents';
+import { insertProjects } from '../js/db/dbEvents.js';
+
+const userProfileString = localStorage.getItem('userProfile');
+const modifiedString = userProfileString.slice(1, -1);
+const user_data = JSON.parse(modifiedString);
+console.log(user_data)
 
 
-document.getElementById("crtProjectBtn").addEventListener('click', function () {
-    let project_name = document.getElementById('projectNameInput' ).value;
-    console.log(`creating a new project, ${project_name}` )
-})
+
+document.getElementById("crtProjectBtn").addEventListener('click', async function () {
+    let project_name = document.getElementById('projectNameInput').value;
+    console.log(`Creating a new project: ${project_name} for user of profile id: ${user_data.profile_uid}`);
+    try {
+        let response = await insertProjects([{ 'profile_uid': user_data.profile_uid , 'project_name': project_name }]);
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    }
+});
